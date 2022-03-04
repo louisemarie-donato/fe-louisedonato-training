fdb3989b7b55398f7a35406677d10a3146ec3f1c
Louise Marie Donato <louise.marie.donato@codeandtheory.com>
2022-03-04 10:54:55 +0800

    FE403: fixed javascript formatting & spacing

--- FE403/index.js
+++ FE403/index.js
@@ -2,93 +2,87 @@ const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
-let matchedCard = document.getElementsByClassName("match");
+let matchedCard = document.getElementsByClassName('match');
 let restart_btn = document.getElementById('restart');
 
 function shuffleDeck() {
-    cards.forEach(card =>
-    {
-        var randomNumber = Math.floor(Math.random() * 16);
-        card.style.order = randomNumber;
-    });
+  cards.forEach((card) => (card.style.order = Math.floor(Math.random() * 16)));
 }
 
 function flip() {
-    if (this === firstCard) return;
-  
-    this.classList.add('flipped');
-  
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        console.log("first:" + firstCard.getAttribute('data-id'));
-        return;
-    }
-    
-    secondCard = this;
-    console.log("second:" + secondCard.getAttribute('data-id'));
-    ifMatch();
+  if (this === firstCard) return;
+
+  this.classList.add('flipped');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    console.log('first:' + firstCard.getAttribute('data-id'));
+    return;
+  }
+
+  secondCard = this;
+  console.log('second:' + secondCard.getAttribute('data-id'));
+  checkMatch();
 }
 
-function unFlip() {
-    setTimeout(()=>
-        {
-            firstCard.classList.remove('flipped');
-            secondCard.classList.remove('flipped');
-            reset();
-        }, 1300
-    );
-}
-
-function disable() {
-    firstCard.removeEventListener('click', flip);
-    secondCard.removeEventListener('click', flip);
-    
+function removeFlip() {
+  setTimeout(() => {
+    firstCard.classList.remove('flipped');
+    secondCard.classList.remove('flipped');
     reset();
+  }, 1300);
+}
+
+function lockCard() {
+  firstCard.removeEventListener('click', flip);
+  secondCard.removeEventListener('click', flip);
+
+  reset();
 }
 
 function reset() {
-    hasFlippedCard = false;
-    firstCard = null;
-    secondCard = null;
+  hasFlippedCard = false;
+  firstCard = null;
+  secondCard = null;
 }
 
-function ifMatch() {
-    let isMatch = firstCard.getAttribute("data-id") === secondCard.getAttribute("data-id");
+function checkMatch() {
+  let isMatch =
+    firstCard.getAttribute('data-id') === secondCard.getAttribute('data-id');
 
-    if (isMatch === true) {
-        firstCard.classList.add('match');
-        secondCard.classList.add('match');
-        disable();
-        console.log('match');
-        console.log(matchedCard);
-    }else{
-        unFlip();
-        
-        console.log('not a match');
-    }
-}
-
-function restart() {
+  if (isMatch === true) {
+    firstCard.classList.add('match');
+    secondCard.classList.add('match');
+    lockCard();
+    console.log('match');
     console.log(matchedCard);
-    if (matchedCard.length === 16){
-        console.log('all match')
-        restart_btn.style.removeProperty("display");
-        restart_btn.style.display = "flex";
-    };
+  } else {
+    removeFlip();
+
+    console.log('not a match');
+  }
+}
+
+function endOfGame() {
+  console.log(matchedCard);
+  if (matchedCard.length === 16) {
+    console.log('all match');
+    restart_btn.style.removeProperty('display');
+    restart_btn.style.display = 'flex';
+  }
 }
 
 function restartGame() {
-    cards.forEach(card => card.classList.remove('flipped'));
-    cards.forEach(card => card.classList.remove('match'));
-    reset();
-    shuffleDeck();
-    console.log('restart');
-    restart_btn.style.display = "none";
+  cards.forEach((card) => card.classList.remove('flipped'));
+  cards.forEach((card) => card.classList.remove('match'));
+  reset();
+  shuffleDeck();
+  console.log('restart');
+  restart_btn.style.display = 'none';
 }
 
-console.log(matchedCard);
 shuffleDeck();
 
-cards.forEach(card => card.addEventListener('click', flip));
-cards.forEach(card => card.addEventListener('click', restart));
+cards.forEach((card) => card.addEventListener('click', flip));
+cards.forEach((card) => card.addEventListener('click', endOfGame));
